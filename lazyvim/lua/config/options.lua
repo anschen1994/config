4b2ae380f26d5a843971b174d3d46e370485a71c
陈宇 <anschen1994@gmail.com>
2023-10-15 18:21:18 +0800

    feat: add conda python

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -10,3 +10,21 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.relativenumber = false
+
+-- custom python provider
+local function isempty(s)
+	return s == nil or s == ""
+end
+
+local function use_if_defined(val, fallback)
+	return val ~= nil and val or fallback
+end
+
+local conda_prefix = os.getenv("CONDA_PREFIX")
+if not isempty(conda_prefix) then
+	vim.g.python_host_prog = use_if_defined(nil, conda_prefix .. "/bin/python")
+	vim.g.python3_host_prog = use_if_defined(nil, conda_prefix .. "/bin/python")
+else
+	vim.g.python_host_prog = use_if_defined(vim.g.python_host_prog, "python")
+	vim.g.python3_host_prog = use_if_defined(vim.g.python3_host_prog, "python3")
+end
